6c30168fd5fd8a7cd3f04796a4dc4ab0ef02cce0
Mauricio Cox <maucox77@gmail.com>
2023-12-07 15:02:31 -0300

    chore: Configuracion y seteo de middlewares en server

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-require('dotenv').config();
 
 const app = express();
 const port = 3000;
 
+// Set EJS para el motor de plantillas
+app.set('view engine', 'ejs');
+
+// Set public static
+app.use(express.static('public'));
+
+// Para que procese la info que enviaremos de un form
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Enviroment Variables con dotEnvjs
+require('dotenv').config({path: './env/.env'});
+
+// Set cookies
+app.use(cookieParser)
 
 // test route
 app.get('/', (request, response) => {
